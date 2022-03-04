2393fdb8891313718b2c00aa7221d534b85cc8aa
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-03-04 13:14:35 +0400

    file created
    file created

---
+++ daysInMonth3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace daysInMonth2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string Month = Console.ReadLine();
+
+            switch (Month)
+            {
+                case "January":
+                case "March":
+                case "May":
+                case "July":
+                case "August":
+                case "October":
+                case "December":
+                    Console.WriteLine("There are 31 days.");
+                    break;
+                case "April":
+                case "June":
+                case "September":
+                case "November":
+                    Console.WriteLine("There are 30 days.");
+                    break;
+                case "February":
+                    Console.WriteLine("There are 28/29 days.");
+                    break;
+                default:
+                    Console.WriteLine("There is no such a Month");
+                    break;
+
+            }
+        }
+    }
+}
